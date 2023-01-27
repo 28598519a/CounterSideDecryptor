69f3ee851c6a3cfa9d0cb04593a49e970adaf323
劃破黑夜 <179971b@gmail.com>
2023-01-28 01:53:42 +0800

    Add files via upload

---
+++ CounterSide/Function/CryptoServices.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace CounterSide
+{
+	class CryptoServices
+	{
+		public string MD5Hash(byte[] input)
+		{
+			MD5 md5 = MD5.Create();
+			byte[] hash = md5.ComputeHash(input);
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < hash.Length; i++)
+			{
+				sb.Append(hash[i].ToString("x2"));
+			}
+
+			return sb.ToString();
+		}
+	}
+}
